e9bfcb9ce5092e77dad26f77e4ff5783d3ff3b5d
vingeophysicist <uobodomavin@gmail.com>
2021-01-01 13:48:05 +0100

    all

---
+++ ebooks/api/serializers.py
@@ -0,0 +1,23 @@
+from ebooks.models import Ebook, Review
+from rest_framework import serializers
+
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    review_author = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Review
+        exclude = ("ebook",)
+#        fields = "__all__"
+
+
+
+class EbookSerializer(serializers.ModelSerializer):
+
+    reviews = ReviewSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Ebook
+        fields = "__all__"
+
